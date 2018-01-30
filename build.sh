56a03b4a4f04f1705703ff704c144ab62210c517
W. Mark Smith <wmsmith@gmail.com>
2018-01-30 00:29:00 -0500

    work in progress - package does not yet build

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PKG=mjpg-streamer
+PKGVER=2018.1
+PKGVERSUB=0
+PKGREL=1
+GITURL=https://github.com/jacksonliam/mjpg-streamer
+
+WD=$(dirname $0)
+[ "$WD" == "." ] && WD=$PWD
+
+. /etc/os-release
+dist=$ID$VERSION_ID
+codename=$VERSION_CODENAME
+[ -z "$codename" ] && codename=$(echo $VERSION | sed 's/.*(\(.*\)).*/\1/')
+
+gitname=$(git config --get user.name)
+gitemail=$(git config --get user.email)
+gitdate=$(date -R)
+
+rm -rf $WD/work
+mkdir $WD/work
+cd $WD/work
+git clone $GITURL
+cd $PKG
+#git checkout v$PKGVER
+cp -r $WD/deb debian
+cat > debian/changelog <<EOF
+$PKG (${PKGVER}.${PKGVERSUB}-${PKGREL}.$dist) $codename; urgency=medium
+
+  * Automatically generated
+
+ -- ${gitname} <${gitemail}>  ${gitdate}
+EOF
+cd $WD/work
+mv $PKG ${PKG}-${PKGVER}.${PKGVERSUB}
+tar czf ${PKG}_${PKGVER}.${PKGVERSUB}.orig.tar.gz ${PKG}-${PKGVER}.${PKGVERSUB}
+
+cat <<EOF
+Source files are prepared. You have two options:
+(1) Build deb package:
+  cd $WD/work/${PKG}-${PKGVER}.${PKGVERSUB} && dpkg-buildpackage -us -uc -b
+(2) Prepare upload for PPA:
+  cd $WD/work/${PKG}-${PKGVER}.${PKGVERSUB} && dpkg-buildpackage -S
+EOF
+
+cd $WD
+ 
